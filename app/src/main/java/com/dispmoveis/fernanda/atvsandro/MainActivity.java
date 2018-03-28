c1ffe3eadfae0804c5b2172bd86f0999ad2abb04
Fernanda <fercrescencio@hotmail.com>
2018-03-27 23:34:22 -0300

    Initial commit

---
+++ app/src/main/java/com/dispmoveis/fernanda/atvsandro/MainActivity.java
@@ -0,0 +1,60 @@
+package com.dispmoveis.fernanda.atvsandro;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+   private Button btn;
+   private TextView quilo;
+   private TextView mensagem;
+   private ImageView img;
+   private int Calcular;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        quilo = findViewById(R.id.idQuilo);
+
+
+        quilo.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() ==   MotionEvent.ACTION_DOWN){
+                    quilo.setText("");
+                }
+
+                return false;
+            }
+        });
+    }
+
+
+    public void Mensagem(View view){
+        quilo = findViewById(R.id.idQuilo);
+        mensagem = findViewById(R.id.txtMensagem);
+        img = findViewById(R.id.img);
+
+        int quilon = Integer.parseInt(quilo.getText().toString());
+        int valor = (quilon * 1000) /500;
+
+        img.setVisibility(View.VISIBLE);
+
+        mensagem.setText("Com "+ quilon+ "kg pode se fazer "+valor+" ovos de 500g.");
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/dispmoveis/fernanda/atvsandro/Pedidos.java
@@ -0,0 +1,88 @@
+package com.dispmoveis.fernanda.atvsandro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Pedidos extends AppCompatActivity {
+
+
+    private EditText nomepedido;
+    private CheckBox creme;
+    private CheckBox chocolate;
+    private TextView quantidade;
+    private TextView pedidos;
+    private Button mais;
+    private Button menos;
+    private Button fazerPedido;
+    private int quant =0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedidos);
+
+        fazerPedido =  findViewById(R.id.btnPedido);
+
+       final Pedido p = new Pedido();
+
+
+        fazerPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Salvar(p);
+            }
+        });
+
+
+    }
+
+
+
+
+   public void Mais(View view){
+
+        quantidade = findViewById(R.id.txtQuantidade);
+
+        quant = quant +1;
+        quantidade.setText(""+quant);
+
+   }
+
+    public void Menos(View view){
+
+        quantidade = findViewById(R.id.txtQuantidade);
+        quant = quant -1;
+        quantidade.setText(""+ quant);
+
+    }
+
+    public void Salvar(Pedido p){
+        pedidos = findViewById(R.id.txtPedido);
+        creme = findViewById(R.id.checkBox);
+        chocolate = findViewById(R.id.checkBox2);
+
+        nomepedido = findViewById(R.id.idPedido);
+        p.setNome(nomepedido.getText().toString());
+        p.setQuantidade(quant);
+
+        if(chocolate.isChecked()){
+            p.setChocolate("Chocolate");
+        }
+        if(creme.isChecked()){
+            p.setCreme("Creme");
+        }
+
+        pedidos.setText(p.getNome()+" , "+p.getCreme()+" , "+p.getChocolate()+" , "+p.getQuantidade());
+
+    }
+
+
+
+}
