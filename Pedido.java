fe44f3eee8ea09a0e4e71b498c4a1552dea48011
Fercrescenciot <37809483+Fercrescenciot@users.noreply.github.com>
2018-04-04 12:08:07 -0300

    Colocando botao nas paginas

---
+++ Pedido.java
@@ -0,0 +1,42 @@
+package com.dispmoveis.fernanda.atvsandro;
+
+public class Pedido {
+
+    private String nome;
+    private String creme;
+    private String chocolate;
+    private int quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getCreme() {
+        return creme;
+    }
+
+    public void setCreme(String creme) {
+        this.creme = creme;
+    }
+
+    public String getChocolate() {
+        return chocolate;
+    }
+
+    public void setChocolate(String chocolate) {
+        this.chocolate = chocolate;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
